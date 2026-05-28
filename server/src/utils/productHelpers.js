b8016c6b769547257add02b60117820fc20b4f01
devjoelraj <dev.joelraj@gmail.com>
2026-05-28 19:31:19 +0530

    model name changed

--- server/src/utils/productHelpers.js
+++ server/src/utils/productHelpers.js
@@ -1,6 +1,6 @@
 import Pants from "../models/admin/addProduct/pants.model.js";
 import Shirts from "../models/admin/addProduct/shirts.model.js";
-import Footwear from "../models/admin/addProduct/footwear.model.js";
+import Footwear from "../models/admin/addProduct/footWear.model.js";
 import Accessories from "../models/admin/addProduct/accessories.model.js";
 
 export const getProductModel = (category) => {
